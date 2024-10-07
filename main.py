e871a4a639f2010847e74375ade84303777702b9
PowFox <lapienis.paulius@yahoo.com>
2024-10-07 15:15:35 +0300

    Added 'add' functionality

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ class Calculator:
     def __init__(self, symbol: str, number: float) -> None:
         self.symbol = symbol
         self.number = number
-    def add(self) -> float:
-        ...
+        
+    def add(self, number) -> float:
+        return self.number + number
+    
     def sub(self) -> float:
         ...
     def div(self) -> float:
